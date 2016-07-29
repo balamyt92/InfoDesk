c2b5217bfada142482b5e585ec7ea433d5a7933f
Anatoly Medvedev <balamyt92@gmail.com>
2016-07-29 20:13:55 +0700

    Подключил дебаг панель

--- config/web.php
+++ config/web.php
@@ -15,7 +15,6 @@ $config = [
             'appendTimestamp' => true,
         ],
     ],
-
 ];
 
 if (YII_ENV_DEV) {
@@ -23,6 +22,12 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class'      => 'yii\gii\Module',
         'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.33.1'], // настройка доступа gii
+    ];
+    // включаем дебаг панель
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.33.1'],
     ];
 }
 
