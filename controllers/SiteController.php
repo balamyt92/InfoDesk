9c67bfafda31e96c5fa79738d4fe644910ab4fd4
Anatoly Medvedev <balamyt92@gmail.com>
2016-07-30 14:34:45 +0700

    Поиск по связанным деатлям

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -135,24 +135,34 @@ class SiteController extends Controller
 
     public function actionSearchParts($detail_id, $mark_id, $model_id, $body_id, $engine_id)
     {
+        $connection = \Yii::$app->getDb();
         $parts = [];
 
+        // ищем все связанные детали
+        $link_detail = "SELECT ID_LinkedDetail from CarENLinkedDetailNames where ID_GroupDetail = :detail_id";
+        $link_detail = $connection->createCommand($link_detail, [':detail_id' => $detail_id,]);
+        $link = $link_detail->queryAll();
+        $tmp = [];
+        foreach ($link as $value)
+        {
+            array_push($tmp, $value['ID_LinkedDetail']);
+        }
+        $link = implode(",", $tmp);
+        $detail_id .= ','.$link;
 
-
+        // запрос результирующеё таблицы
         $sql = "SELECT DETAIL.Name as DetailName, MARK.Name as MarkName, MODEL.Name as ModelName, ".
-               "BODY.Name as BodyName, ENGINE.Name as EngineName, A.CarYear, A.Comment, ".
-               "A.Cost, A.Catalog_Number, A.TechNumber, A.ID_Firm ".
-               "FROM CarPresenceEN AS A ".
-               "LEFT JOIN CarENDetailNames AS DETAIL ON (DETAIL.id=A.ID_Name) ".
-               "LEFT JOIN CarMarksEN as MARK ON (MARK.id=A.ID_Mark) ".
-               "LEFT JOIN CarModelsEN as MODEL ON (MODEL.id=A.ID_Model) ".
-               "LEFT JOIN CarBodyModelsEN as BODY ON (BODY.id=A.ID_Body) ".
-               "LEFT JOIN CarEngineModelsEN as ENGINE ON (ENGINE.id=A.ID_Engine) ".
-               "LEFT JOIN Firms ON (Firms.id=A.ID_Firm) ".
-               "WHERE A.ID_Name=:detail_id AND A.ID_Mark=:mark_id AND Firms.Enabled=1 ";
-
-        $map = [':detail_id' => $detail_id,
-                ':mark_id' => $mark_id,];
+            "BODY.Name as BodyName, ENGINE.Name as EngineName, A.CarYear, A.Comment, ".
+            "A.Cost, A.Catalog_Number, A.TechNumber, A.ID_Firm ".
+            "FROM CarPresenceEN AS A ".
+            "LEFT JOIN CarENDetailNames AS DETAIL ON (DETAIL.id=A.ID_Name) ".
+            "LEFT JOIN CarMarksEN as MARK ON (MARK.id=A.ID_Mark) ".
+            "LEFT JOIN CarModelsEN as MODEL ON (MODEL.id=A.ID_Model) ".
+            "LEFT JOIN CarBodyModelsEN as BODY ON (BODY.id=A.ID_Body) ".
+            "LEFT JOIN CarEngineModelsEN as ENGINE ON (ENGINE.id=A.ID_Engine) ".
+            "LEFT JOIN Firms ON (Firms.id=A.ID_Firm) ".
+            "WHERE A.ID_Name IN ({$detail_id}) ".
+            "AND A.ID_Mark=:mark_id AND Firms.Enabled=1 ";
 
         if(!($model_id === "false")) {
             $sql .= "AND A.ID_Model=:model_id ";
@@ -165,13 +175,13 @@ class SiteController extends Controller
         if(!($engine_id === "false")) {
             $sql .= "AND A.ID_Engine=:engine_id ";
             $map[':engine_id'] = $engine_id;
-        }       
+        }
 
-
-        $connection = \Yii::$app->getDb();
+        $map = [
+            ':mark_id' => $mark_id,
+        ];
         $command = $connection->createCommand($sql, $map);
         $parts = $command->queryAll();
-        // $parts = CarPresenceEN::findBySql($sql)->all();
 
 
         \Yii::$app->response->format = Response::FORMAT_JSON;
