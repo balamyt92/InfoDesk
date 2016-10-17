ae90a7476cba10a8a2eac0c054d978c63bce5443
Anatoly Medvedev <balamyt92@gmail.com>
2016-10-17 19:50:12 +0700

    clean

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,7 +37,6 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Call-center', 'url' => ['/site/index']],
-        ['label' => 'Импорт', 'url' => ['/import/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
