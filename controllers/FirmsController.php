ff408b43903ec1c3fe6c34285bb60040b23994ea
Anatoly Medvedev <balamyt92@gmail.com>
2016-12-06 23:56:45 +0700

    fix redirect after create\update

--- controllers/FirmsController.php
+++ controllers/FirmsController.php
@@ -102,7 +102,7 @@ class FirmsController extends Controller
         $model = new Firms();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -123,7 +123,7 @@ class FirmsController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         } else {
             return $this->render('update', [
                 'model' => $model,
