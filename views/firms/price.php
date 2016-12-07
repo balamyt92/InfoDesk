9630860c407d1dee58af46893ff55c7a7ffe1730
Anatoly Medvedev <balamyt92@gmail.com>
2016-12-07 16:09:41 +0700

    settings grid price and service

--- views/firms/price.php
+++ views/firms/price.php
@@ -2,16 +2,50 @@
 use kartik\grid\GridView;
 use yii\helpers\Html;
 use yii\widgets\Pjax;
+use kartik\export\ExportMenu;
 
 $this->title = 'Прайс фирмы';
 ?>
 
 <div class="price-firm">
-	<?= Html::a('Отмена', 'javascript:history.back()', ['class' => 'btn btn-warning']) ?>
-
 	<?php Pjax::begin(); ?>    
 	<?= GridView::widget([
 		'dataProvider' => $model,
+		'panel'         => [
+            'type' => 'default',
+        ],
+        'pager'         => [
+            'firstPageLabel' => 'Перв.',
+            'lastPageLabel'  => 'Послед.',
+            'prevPageLabel'  => 'Пред.',
+            'nextPageLabel'  => 'След.',
+            'maxButtonCount' => 20,
+        ],
+        'toolbar'       => [
+            "<span class=\"btn-group\">{summary}</span>",
+            "<span class=\"btn-group\">" . Html::a('Назад', 'javascript:history.back()', ['class' => 'btn btn-warning']) . "</span>",
+            ExportMenu::widget([
+			    'dataProvider' 	  => $model,
+			    'fontAwesome' 	  => true,
+			    'target' 		  => ExportMenu::TARGET_SELF,
+			    'dropdownOptions' => [
+			        'label' => 'Экспорт прайса',
+			        'class' => 'btn btn-default'
+			    ],
+                'pdfLibrary' => PHPExcel_Settings::PDF_RENDERER_MPDF,
+			    'pdfLibraryPath' => '@vendor/mpdf/mpdf',
+			]),
+			'{export}',
+            '{toggleData}',
+        ],
+        'panelTemplate' => "
+            <div class=\"{prefix}{type}\">
+                {panelBefore}
+                {items}
+                {panelAfter}
+                {panelFooter}
+            </div>
+        ",
 	]);?>
 	<?php Pjax::end();?>
 
--- views/firms/service.php
+++ views/firms/service.php
@@ -7,11 +7,33 @@ $this->title = 'Услуги фирмы';
 ?>
 
 <div class="service-firm">
-	<?= Html::a('Отмена', 'javascript:history.back()', ['class' => 'btn btn-warning']) ?>
-
 	<?php Pjax::begin(); ?>    
 	<?= GridView::widget([
 		'dataProvider' => $model,
+		'panel'         => [
+            'type' => 'default',
+        ],
+        'pager'         => [
+            'firstPageLabel' => 'Перв.',
+            'lastPageLabel'  => 'Послед.',
+            'prevPageLabel'  => 'Пред.',
+            'nextPageLabel'  => 'След.',
+            'maxButtonCount' => 20,
+        ],
+        'toolbar'       => [
+            "<span class=\"btn-group\">{summary}</span>",
+            "<span class=\"btn-group\">" . Html::a('Назад', 'javascript:history.back()', ['class' => 'btn btn-warning']) . "</span>",
+            '{export}',
+            '{toggleData}',
+        ],
+        'panelTemplate' => "
+            <div class=\"{prefix}{type}\">
+                {panelBefore}
+                {items}
+                {panelAfter}
+                {panelFooter}
+            </div>
+        ",
 	]);?>
 	<?php Pjax::end();?>
 
